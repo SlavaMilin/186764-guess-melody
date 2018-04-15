11fb889436faa31f829122d641f9d8a1c144b817
Slava Milin <slava.milin@gmail.com>
2018-04-15 18:51:35 +0300

    add count answer function

--- js/core/countStatistic.js
+++ js/core/countStatistic.js
@@ -1,5 +1,17 @@
-const countStatistic = (data, currentScreen) => {
-  return currentScreen;
+const countStatistic = (getAnswer, spendTime, data) => {
+  if (!data) {
+    data = {};
+    data.answers = [];
+    data.screen = 0;
+  }
+  const result = {};
+  result.screen = data.screen + 1;
+  result.answers = [{
+    correct: getAnswer,
+    time: spendTime
+  }, ...data.answers];
+
+  return result;
 };
 
 export {countStatistic};
--- js/core/countStatistic.test.js
+++ js/core/countStatistic.test.js
@@ -2,18 +2,27 @@ import {assert} from 'chai';
 import {countStatistic} from "./countStatistic";
 
 const testData = {
-  currentScreen: 4,
-  answer: [
-
+  screen: 1,
+  answers: [
+    {
+      correct: true,
+      time: 25
+    }
   ]
 };
 
 describe(`test statistic function`, () => {
-  it(`should return array`, () => {
-    assert.isArray(countStatistic());
+  it(`should return object`, () => {
+    assert.isObject(countStatistic(true, 25));
   });
-  it(`should increase length by 1 after call`, () => {
-    const current = countStatistic.length;
-    assert.equal(countStatistic().length, current + 1);
+  it(`should increase number of screen by 1 after call`, () => {
+    const current = countStatistic(true, 25, testData);
+    const currentTestScreen = current.screen;
+    assert.equal(countStatistic(true, 25, current).screen, currentTestScreen + 1);
+  });
+  it(`should return basic parameters if func call without data`, () => {
+    const current = countStatistic(true, 25);
+    assert.equal(current.screen, 1);
+    assert.equal(current.answers.length, 1);
   });
 });
